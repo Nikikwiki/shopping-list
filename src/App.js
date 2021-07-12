aa36b9677de9f841c30e26b67c3ddb84c70498e7
Nikita <nikikwiki@mail.ru>
2021-07-12 18:02:55 +0300

    Add material theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 
 function App() {
-  const greeting = "greeting";
   return (
     <div className="container">
-      <h1 id={greeting} >Hello, World</h1>
-      <p>I am writing JSX</p>
     </div>
   )
 }
