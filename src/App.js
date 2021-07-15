9b65d587e7291e63a11ca857988c6dcbb967707c
Nikita <nikikwiki@mail.ru>
2021-07-15 11:47:08 +0300

    Add input into header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import { Button } from '@material-ui/core';
 import React, { useState } from 'react';
+
 import ToDo from './ToDo';
 import ToDoForm from './ToDoForm';
-import {Button, TextField, InputAdornment, AppBar, Toolbar, InputBase } from '@material-ui/core';
-import { Search } from '@material-ui/icons';
-
+import HeaderBar from './HeaderBar';
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -59,34 +59,9 @@ function App() {
     }
   }
 
-  const SearchInput = (props) => {
-    return (
-      <div className="search-input">
-      <TextField 
-        label="Поиск" 
-        variant="outlined" 
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <Search />
-            </InputAdornment>
-          )
-        }}
-      />
-
-      </div>
-    )
-  }
-
   return (
     <div className="app">
-
-    <AppBar position="static">
-      <Toolbar>
-        <SearchInput className="search-input__icon"/>
-      </Toolbar>
-    </AppBar>
-
+      <HeaderBar/>
       <Creation />
       <div className="app__todos">
         {todos.map((todo) => {
